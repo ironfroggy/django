bc38de0d13c25ba00c4374da5e370757afe823be
Calvin Spealman <ironfroggy@gmail.com>
2011-11-12 19:51:40 +0000

    [17143] Expanding test into 4 different failure conditions, 3 of which fail currently.

--- tests/modeltests/test_17143/tests.py
+++ tests/modeltests/test_17143/tests.py
@@ -11,15 +11,26 @@ from .models import A, B, init_effect
 
 class ModelTest(TestCase):
 
-    def _clear_init_effect(self):
+    def setUp(self):
+        self.b = B.objects.create(foo='Y', bar='Z')
+
+    def tearDown(self):
         init_effect[:] = []
 
-    def test_17143(self):
-        b = B.objects.create(foo='Y', bar='Z')
+    def test_17143_1(self):
         self.assertEqual(init_effect, ['Y'])
-        self._clear_init_effect()
 
-        (a,) = A.objects.select_related('b')
-        self.assertEqual(a.id, b.id)
+    def test_17143_2(self):
+        a = A.objects.get()
         self.assertEqual(init_effect, ['Y'])
-        self._clear_init_effect()
+
+    def test_17143_3(self):
+        a = A.objects.get()
+        b = a.b
+        self.assertEqual(b.foo, a.foo)
+        self.assertEqual(init_effect, ['Y'])
+
+    def test_17143_4(self):
+        (a,) = A.objects.select_related('b')
+        self.assertEqual(a.id, self.b.id)
+        self.assertEqual(init_effect, ['Y'])
